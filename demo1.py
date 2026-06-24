415a79e4abaa17b299fe34273ca6a1c6a3efcab9
Nkosinathi Emmanuel <nkosinathingcobo123@gmail.com>
2026-06-24 13:03:56 +0200

    Initial commit

--- demo1.py
+++ demo1.py
@@ -1,13 +1,28 @@
 """..."""
 import os
+import string
 import time
+import random
 import csv
+import sys
 
 # This is a sample Python script.
-
+chars = string.ascii_letters + string.digits + string.punctuation
 # print('hello world')
-
+password = ''.join(random.choice(chars) for i in range(12))
+username = ''.join(random.choice(string.ascii_lowercase +
+                   string.digits) for i in range(8))
 # method to countdown from a given number of seconds
+
+print(f'Your generated username is: {username}')
+print(f'Your generated password is: {password}')
+
+
+def countd(sec):
+    while sec >= 0:
+        print(f'\r{sec} seconds remaining', end='', flush=True)
+        time.sleep(1)
+        sec -= 1
 
 
 with open('users.csv', 'r') as file:
@@ -41,13 +56,19 @@ def menu(choice):
         print("You selected to login again")
         print("Please enter your username and password to login.")
         username = input("Enter your username: ")
-        password = input("Enter your password: ")
-        cpj(username)
+        print(cpj(username))
+    elif choice == '2':
+        print('duh')
+    elif choice == '3':
+        print("You selected to check if a user has created a password.")
+        username = input("Enter your username: ")
+        message = cpj(username)
+        print(message)
+    elif choice == '4':
+        print("Exiting the program. Goodbye!")
+        sys.exit()
     else:
         print("Invalid choice. Please try again.")
-
-
-choice = input("Enter your choice (1 to login again): ")
 
 
 def cpj(username):
@@ -57,19 +78,57 @@ def cpj(username):
         for row in reader:
             if row['Username'.strip()] == username:
                 if row['Password'.strip()]:
-                    return 'Password exists.'
+                    print('Enter your password to login.')
+                    password = input("Enter your password: ")
+                    if row['Password'.strip()] == password:
+                        check_password(password)
+                        return 'Login successful.'
                 return 'Password does not exist.'
         return 'Username does not exist.'
 
 
-username = input("Enter a username to check: ")
-# password = input("Enter the password: ")
-print(check_username(username))
-message = cpj(username)
-print('nhghhhggfvtrggvtggvrf')
-print(message)
+def validate_password(password):
+    if len(password) < 8:
+        return False
+    elif not any(char.isdigit() for char in password):
+        return False
 
-menu(choice)
+
+"""if len(password) < 8:
+        return False
+        if not any(char.isdigit() for char in password):
+            return False
+        if not any(char.isupper() for char in password):
+            return False
+        if not any(char.islower() for char in password):
+            return False
+        if not any(char in "!@#$%^&*()-_=+[{]}\|;:'\",<.>/?`~" for char in password):
+            return False
+        return True"""
+
+
+def options():
+    print("Welcome to the Ngcobo login system.\nSelect 1 if you are a returning user.\nSelect 2 if you are a new user and want to register.\nSelect 3 if you want to exit the program.")
+    choice = input("Enter your choice: ")
+    if choice not in ['1', '2', '3']:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Invalid choice. Please try again.")
+        return options()
+    return choice
+
+
+# print("Welcome to the Ngcobo login system.\nSelect 1 if you are a returning user.\nSelect 2 if you are a new user and want to register.\nSelect 3 if you want to exit the program.")
+# choice = input("Enter your choice: ")
+print(options())
+
+menu(options())
+# username = input("Enter a username to check: ")
+# password = input("Enter the password: ")
+# print(check_username(username))
+# message = cpj(username)
+print('nhghhhggfvtrggvtggvrf')
+# print(message)
+
 
 """if check_username(username):
     print("Username exists.")
@@ -96,13 +155,6 @@ for i in range(5, -1, -1):
     time.sleep(1)
 print("Time's up!")
 """
-
-
-def countd(sec):
-    while sec >= 0:
-        print(f'\r{sec} seconds remaining', end='', flush=True)
-        time.sleep(1)
-        sec -= 1
 
 
 countd(5)
